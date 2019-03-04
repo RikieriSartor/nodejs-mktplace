5f7b846b8fe060f686a5ccf4f07862ae78782ee4
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-04 09:35:22 -0300

    Adicionando Mongoose

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dbConfig = require('./config/database')
 
 class App {
   constructor() {
     this.express = express()
     this.isDev = process.env.NODE_ENV === 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
   }
@@ -15,6 +18,13 @@ class App {
 
   routes() {
     this.express.use(require('./routes'))
+  }
+
+  database() {
+    mongoose.connect(dbConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 }
 
