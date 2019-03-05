86f72dc298564759043622aa585bec803bc4d0c4
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-05 16:56:41 -0300

    Adicionando template de email

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -16,7 +16,12 @@ class PurchaseController {
       from: '"Diego Fernandes" <diego.fernandes@rocketseat.com.br>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${ purchaseAd.title }`,
-      html: `<p>${ content }</p>`
+      template: 'purchase',
+      context: {
+        user,
+        content,
+        ad: purchaseAd
+      }
     })
 
     return res.send()
