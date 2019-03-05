14fe0ac1dd75b475f1d3e3b289bd690b0d6c70ad
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-05 17:57:18 -0300

    Adicionando Redis e Jobs para e-mails

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,6 +1,7 @@
 const Ad = require('../models/Ad')
 const User = require('../models/User')
-const Mail = require('../services/Mail')
+const PurchaseMail = require('../jobs/PurchaseMail')
+const Queue = require('../services/Queue')
 
 class PurchaseController {
   async store(req, res) {
@@ -12,20 +13,13 @@ class PurchaseController {
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
 
-    await Mail.sendMail({
-      from: '"Diego Fernandes" <diego.fernandes@rocketseat.com.br>',
-      to: purchaseAd.author.email,
-      subject: `Solicitação de compra: ${ purchaseAd.title }`,
-      template: 'purchase',
-      context: {
-        user,
-        content,
-        ad: purchaseAd
-      }
-    })
+    Queue.create(PurchaseMail.key, {
+      ad: purchaseAd,
+      user,
+      content
+    }).save()
 
     return res.send()
-
   }
 }
 
