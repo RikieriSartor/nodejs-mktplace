8b300be5d00ce7e2d04ceb57694b273654b0e2c1
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-05 22:40:59 -0300

    Adicionando Validators

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,27 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post('/users', validate(validators.User), controllers.UserController.store)
+routes.post('/sessions', validate(validators.Session), controllers.SessionController.store)
 
 routes.use(authMiddleware)
 
 //Ads routes
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 //Purchase routes
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post('/purchase', validate(validators.Purchase), controllers.PurchaseController.store)
 
 
 module.exports = routes
