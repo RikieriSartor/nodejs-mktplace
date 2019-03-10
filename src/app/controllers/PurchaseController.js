a83c1095be061173bd567a5782fcbedb61ff9e86
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-10 11:27:50 -0300

    Adicionando Purchase Model

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,4 +1,5 @@
 const Ad = require('../models/Ad')
+const Purchase = require('../models/Purchase')
 const User = require('../models/User')
 const PurchaseMail = require('../jobs/PurchaseMail')
 const Queue = require('../services/Queue')
@@ -13,13 +14,29 @@ class PurchaseController {
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
 
+    const approvedPurchase = await Purchase.findOne({
+      status: 'A'
+    })
+
+    if (approvedPurchase) {
+      return res.status(400).json({
+        message: `Ad ${ ad } is already closed`
+      })
+    }
+
     Queue.create(PurchaseMail.key, {
       ad: purchaseAd,
       user,
       content
     }).save()
 
-    return res.send()
+    const purchase = await Purchase.create({
+      ad,
+      user: user.id,
+      content
+    })
+
+    return res.json(purchase)
   }
 }
 
